27fab28cf170091ebf1fa1fc11637a09a7c5467c
Tim <tbrandle53@gmail.com>
2017-05-09 21:45:53 -0600

    a lot of stuff

--- elevator.js
+++ elevator.js
@@ -1,7 +1,156 @@
 export default class Elevator {
   constructor() {
+    this.direction = "up" || this.setDirection()
+    this.motionStatus = "idle"
+    this.currentFloor = 0
+    this.currentRiders= {
+      up:[],
+      down:[]
+    }
+    this.request = {
+      up:[],
+      down:[]
+    }
+    this.lastStop = 0
+    this.floorsTraversed = 0
   }
 
-  get currentFloor() {
+  reset(){
+    this.direction = "up"
+    this.motionStatus = 'idle'
+    this.currentFloor = 0
+    this.currentRiders= {
+      up:[],
+      down:[]
+    }
+    this.lastStop = 0
+    this.floorsTraversed = 0
   }
+
+  /**********************/
+
+  addRider(user){
+    user.currentFloor < user.dropOffFloor
+      ? this.currentRiders.up.push(user)
+      : this.currentRiders.down.push(user)
+
+    this.setDirection()
+
+    this.currentFloor = user.currentFloor
+    this.sortRiders()
+    this.getLastStop()
+  }
+
+  sortRiders(){
+    const direction = this.direction
+    this.currentRiders.up.sort((a, b) =>{
+      return a.dropOffFloor - b.dropOffFloor
+    })
+    this.currentRiders.down.sort((a, b) =>{
+      return b.dropOffFloor - a.dropOffFloor
+    })
+  }
+
+  setDirection(){
+    if (this.currentFloor === this.lastStop) {
+      if (!this.currentRiders.up.length && this.currentRiders.down.length) {
+        this.direction = "down"
+      }
+      if (!this.currentRiders.down.length && this.currentRiders.up.length) {
+        this.direction = "up"
+      }
+    }
+  }
+
+  /**********************/
+
+  goToFloor(){
+    const direction = this.direction
+    // this.getFloors()
+    this.currentFloor = this.currentRiders[direction][0].dropOffFloor
+    this.currentRiders[direction].shift()
+
+  }
+
+  getFloors(){
+    // this.floorsTraversed = this.currentFloor
+    const stops = this.getStops()
+    const max = Math.max(...stops)
+    const min = Math.min(...stops)
+
+    // console.log("stops", stops);
+    console.log("max - min: ", max - min);
+
+    this.floorsTraversed = Math.abs(min - max)
+    console.log("floorsTraversed: ", this.floorsTraversed);
+
+    // return max - min + this.currentFloor;
+  }
+
+  /**********************/
+
+  getLastStop(){
+    this.direction === "up"
+      ? this.lastStop = Math.max(...this.getStops())
+      : this.lastStop = Math.min(...this.getStops())
+  }
+
+  getStops(){
+    const { up, down } = this.currentRiders
+    return this.direction === "up"
+      ? this.getStopsReducer(up).sort((a, b) => {
+        return a-b
+      })
+      : this.getStopsReducer(down).sort((a, b) => {
+        return a-b
+      })
+  }
+
+  getStopsReducer(obj){
+    return obj.reduce((stopsArray, user) => {
+      !stopsArray.includes(user.currentFloor) && stopsArray.push(user.currentFloor)
+      !stopsArray.includes(user.dropOffFloor) && stopsArray.push(user.dropOffFloor)
+      return stopsArray
+    },[])
+  }
+
+}
+
+
+// riderRequest(user){
+//
+// if (this.direction === "up" && this.currentFloor > user.currentFloor || this.direction === "down" && this.currentFloor < user.currentFloor){
+//     this.queRider(user)
+//   } else if(this.direction === "down")
+//   this.addRider(user)
+// }
+//
+// queRider(user) {
+//   user.currentFloor < user.dropOffFloor
+//     ? this.queRiders.up.push(user)
+//     : this.queRiders.down.push(user)
+// }
+//
+// addRider(user){
+//   user.currentFloor < user.dropOffFloor
+//     ? this.currentRiders.up.push(user)
+//     : this.currentRiders.down.push(user)
+//
+//   this.setDirection()
+//
+//   this.currentFloor = user.currentFloor
+//   this.sortRiders()
+//   this.getLastStop()
+// }
+
+
+class Person {
+  constructor() {
+    this.name = name,
+    this.currentFloor = currentFloor,
+    this.dropOffFloor = dropOffFloor,
+    this.direction = 'up' || 'down'
+  }
+
+
 }
--- tests/elevator_test.js
+++ tests/elevator_test.js
@@ -2,6 +2,10 @@ require('babel-core/register')({
   ignore: /node_modules\/(?!ProjectB)/
 });
 
+require('locus');
+
+
+const assert = require('chai').assert;
 const Elevator = require('../elevator').default;
 
 describe('Elevator', function() {
@@ -12,20 +16,149 @@ describe('Elevator', function() {
   });
 
   it('should bring a rider to a floor above their current floor', () => {
-    let mockUser = { name: "Brittany", currentFloor: 2, dropOffFloor: 5 };
-    elevator.goToFloor(mockUser);
+    const mockUser1 = { name: "Brittany", currentFloor: 3, dropOffFloor: 8 };
 
-    assert(elevator.currentFloor).equals(5);
-    assert(elevator.motionStatus).equals('idle');
-    assert(elevator.getStops()).equals([2, 5]);
+    elevator.addRider(mockUser1);
+
+    assert.deepEqual(elevator.currentRiders.up.length, 1)
+
+    elevator.goToFloor()
+
+    assert.deepEqual(elevator.currentFloor, 8);
+    assert.deepEqual(elevator.motionStatus, 'idle');
+    assert.deepEqual(elevator.lastStop, 8)
   });
+
+  it('should sort the up and down arrays by dropOffFloor from least to greatest', () => {
+    const mockUser1 = { name: "Brittany", currentFloor: 3, dropOffFloor: 8 };
+    const mockUser2 = { name: "Tim", currentFloor: 4, dropOffFloor: 9 };
+    const mockUser3 = { name: "Bill", currentFloor: 2, dropOffFloor: 4 };
+
+    elevator.addRider(mockUser1);
+    elevator.addRider(mockUser2);
+    elevator.addRider(mockUser3);
+
+    const result = [ { name: 'Bill', currentFloor: 2, dropOffFloor: 4 },
+                     { name: 'Brittany', currentFloor: 3, dropOffFloor: 8 },
+                     { name: 'Tim', currentFloor: 4, dropOffFloor: 9 } ]
+
+    assert.deepEqual(elevator.currentRiders.up, result)
+  })
+
+  it('should shift out the first person from the up array once they have been dropped off', () => {
+    const mockUser1 = { name: "Brittany", currentFloor: 3, dropOffFloor: 8 };
+    const mockUser2 = { name: "Tim", currentFloor: 4, dropOffFloor: 9 };
+    const mockUser3 = { name: "Bill", currentFloor: 2, dropOffFloor: 4 };
+
+    elevator.addRider(mockUser1);
+    elevator.addRider(mockUser2);
+    elevator.addRider(mockUser3);
+
+    const before = [ { name: 'Bill', currentFloor: 2, dropOffFloor: 4 },
+                     { name: 'Brittany', currentFloor: 3, dropOffFloor: 8 },
+                     { name: 'Tim', currentFloor: 4, dropOffFloor: 9 } ]
+
+    assert.deepEqual(elevator.currentRiders.up, before)
+
+    elevator.goToFloor()
+
+    const after = [ { name: 'Brittany', currentFloor: 3, dropOffFloor: 8 },
+                    { name: 'Tim', currentFloor: 4, dropOffFloor: 9 } ]
+
+    assert.deepEqual(elevator.currentRiders.up, after)
+  })
 
   it('should bring a rider to a floor below their current floor', () => {
-    let mockUser = { name: "Brittany", currentFloor: 8, dropOffFloor: 3 };
-    elevator.goToFloor(mockUser);
+    const mockUser = { name: "Brittany", currentFloor: 8, dropOffFloor: 3 };
+    elevator.addRider(mockUser);
 
-    assert(elevator.currentFloor).equals(3);
-    assert(elevator.motionStatus).equals('idle');
-    assert(elevator.getStops()).equals([8, 3]);
+    assert.deepEqual(elevator.currentRiders.down.length, 1)
+    assert.deepEqual(elevator.currentFloor, 8)
+
+    // eval(locus)
+
+    elevator.goToFloor()
+    assert.deepEqual(elevator.currentFloor, 3);
+    assert.deepEqual(elevator.currentRiders.down.length, 0)
   });
+
+  it('should keep track of all the stops in one direction', () =>{
+    const mockUser1 = { name: "Brittany", currentFloor: 3, dropOffFloor: 8 };
+    const mockUser2 = { name: "Tim", currentFloor: 4, dropOffFloor: 9 };
+    elevator.addRider(mockUser1);
+    elevator.addRider(mockUser2);
+    assert.deepEqual(elevator.getStops(), [3, 4, 8, 9]);
+  })
+
+  it('should drop off everyone in the up array, then drop of the down array', () =>{
+    const mockUserUp1 = { name: "Brittany", currentFloor: 3, dropOffFloor: 8 };
+    const mockUserUp2 = { name: "Tim", currentFloor: 5, dropOffFloor: 10 };
+    const mockUserUp3 = { name: "Tim", currentFloor: 6, dropOffFloor: 13 };
+
+    const mockUserDown1 = { name: "Brittany", currentFloor: 12, dropOffFloor: 8 };
+    const mockUserDown2 = { name: "Tim", currentFloor: 10, dropOffFloor: 5 };
+    const mockUserDown3 = { name: "Tim", currentFloor: 4, dropOffFloor: 2 };
+
+    elevator.addRider(mockUserUp1);
+    elevator.addRider(mockUserUp2);
+    elevator.addRider(mockUserUp3);
+
+    elevator.addRider(mockUserDown1);
+    elevator.addRider(mockUserDown2);
+    elevator.addRider(mockUserDown3);
+
+    assert.deepEqual(elevator.direction, "up");
+
+    elevator.goToFloor()
+    elevator.goToFloor()
+    elevator.goToFloor()
+
+    assert.deepEqual(elevator.direction, "down");
+    assert.deepEqual(elevator.currentRiders.up.length, 0);
+    assert.deepEqual(elevator.currentRiders.down.length, 3);
+
+    elevator.goToFloor()
+    elevator.goToFloor()
+    elevator.goToFloor()
+
+    assert.deepEqual(elevator.currentRiders.up.length, 0);
+    assert.deepEqual(elevator.currentRiders.down.length, 0);
+  })
+
+
+
+  it.skip('should be able to keep track of how many total floors', () => {
+    const mockUser1 = { name: "Brittany", currentFloor: 3, dropOffFloor: 8 };
+    const mockUser2 = { name: "Tim", currentFloor: 5, dropOffFloor: 10 };
+    const mockUser3 = { name: "Tim", currentFloor: 5, dropOffFloor: 13 };
+    elevator.addRider(mockUser1);
+    elevator.addRider(mockUser2);
+    elevator.addRider(mockUser3);
+    assert.deepEqual(elevator.getStops(), [3, 5, 8, 10, 13]);
+
+    // eval(locus)
+    elevator.goToFloor()
+    elevator.goToFloor()
+    elevator.goToFloor()
+
+
+    assert.deepEqual(elevator.floorsTraversed, 13)
+
+  })
+
+  it.skip('should store a collection of requests and a collection of current riders on the elevator.', () => {
+    const mockUser1 = { name: "Brittany", currentFloor: 8, dropOffFloor: 3 };
+    const mockUser2 = { name: "Tim", currentFloor: 10, dropOffFloor: 5 };
+    elevator.goToFloor(mockUser1);
+    elevator.goToFloor(mockUser2);
+
+    assert.deepEqual(elevator.currentRiders.length, 2)
+  })
+
+  it.skip('should', () => {
+
+  })
+
+
+
 });
